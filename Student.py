3a05e6ccce5e4bec7cdbc3b2beab6a8a176a8a3a
SavindaJayasekara <thantrige32@gmail.com>
2023-06-02 11:37:18 +0530

    aded components !

--- Student.py
+++ Student.py
@@ -1,8 +1,14 @@
 import string
 
 
-class Student:
+class Person:
+    def __int__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+
+class Student(Person):
     def __int__(self, name, age):
         self.name = name
         self.age = age
-
